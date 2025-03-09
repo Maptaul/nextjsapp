2db5c6420350c304bd9dcb0aa2deb8e0c87f828d
@maptaul <maptaulislam1@gmail.com>
2025-03-09 23:58:31 +0600

    ward add

--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,18 @@
 import { NextResponse } from "next/server";
+import { is } from "./../.next/static/chunks/[root of the server]__31723f._";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-  console.log("pathName", request.nextUrl.pathName.startswith("/services"));
+  const currentCookie = request.cookies.get("next-js");
+  console.log("currentCookie", currentCookie);
+  const dummyUserData = {
+    role: "user",
+    email: "xyz@gmail.com",
+  };
+  let isServicesPage = request.nextUrl.pathname.startsWith("/services");
+  let isAdmin = dummyUserData.role === "admin";
+  if (isServicesPage && !isAdmin) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
   return NextResponse.next();
 }
-U;
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: "/about/:path*",
-};
